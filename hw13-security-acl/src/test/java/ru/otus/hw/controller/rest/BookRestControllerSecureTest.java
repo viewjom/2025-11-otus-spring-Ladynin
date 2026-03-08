5347c48d69352dc68c26793ac599bdee15e2687f
ladynin <ladynin@komus.net>
2026-03-09 02:43:03 +0300

    hw13-security-acl fixing_6

--- hw13-security-acl/src/test/java/ru/otus/hw/controller/rest/BookRestControllerSecureTest.java
+++ hw13-security-acl/src/test/java/ru/otus/hw/controller/rest/BookRestControllerSecureTest.java
@@ -1,10 +1,7 @@
 package ru.otus.hw.controller.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +13,8 @@ import ru.otus.hw.Application;
 import ru.otus.hw.dto.AuthorDto;
 import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.GenreDto;
-import org.json.JSONObject;
-import org.json.JSONException;
-import ru.otus.hw.services.BookServiceImpl;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,20 +33,18 @@ class BookRestControllerSecureTest {
     @DisplayName("должен загружать книги")
     @ParameterizedTest
     @CsvSource({
-              "/api/books, admin, ROLE_ADMIN, 200",
-              "/api/books/1, user1, ROLE_AUTHOR1, 200",
-              "/api/books/2, user2, ROLE_AUTHOR2, 200",
-              "/api/books/3, user3, ROLE_AUTHOR3, 200",
-              "/api/books/1, user2, ROLE_AUTHOR2, 403",
-              "/api/books/2, user3, ROLE_AUTHOR3, 403",
-              "/api/books/3, user1, ROLE_AUTHOR1, 403"
+            "/api/books, admin, ROLE_ADMIN, 200",
+            "/api/books/2, user2, ROLE_AUTHOR2, 200",
+            "/api/books/1, user1, ROLE_AUTHOR1, 200",
+            "/api/books/3, user3, ROLE_AUTHOR3, 200",
+            "/api/books/1, user2, ROLE_AUTHOR2, 403",
+            "/api/books/2, user3, ROLE_AUTHOR3, 403",
+            "/api/books/3, user1, ROLE_AUTHOR1, 403"
     })
     void shouldGetBookSecureReturn(String url, String user, String role, int result) throws Exception {
-      var res = mvc.perform(get(url)
+        mvc.perform(get(url)
                         .with(user(user).authorities(new SimpleGrantedAuthority(role))))
                 .andExpect(status().is(result)).andReturn();
-
-        res.getRequest().logout();
     }
 
     @DisplayName("должен сохранять измененную книгу")
@@ -63,7 +54,9 @@ class BookRestControllerSecureTest {
             "/api/books, admin, ROLE_ADMIN, 200, Test2"
     })
     void shouldSaveNewBook(String url, String user, String role, int result, String title) throws Exception {
-        BookDto bookDto = getTestListBookDto().get(0);
+        BookDto bookDto = new BookDto(1L, "BookTitle_1",
+                new AuthorDto(1L, "Author_1"),
+                new GenreDto(1L, "Genre_1"));
         bookDto.setTitle(title);
         bookDto.setAuthorDto(new AuthorDto(2L, "Author_2"));
         bookDto.setGenreDto(new GenreDto(2L, "Genre_2"));
@@ -105,24 +98,8 @@ class BookRestControllerSecureTest {
             "/api/books/3, admin, ROLE_ADMIN, 200"
     })
     void shouldDeleteBook(String url, String user, String role, int result) throws Exception {
-        BookDto expectedBookDto = getTestListBookDto().get(0);
-        String expectedResult = mapper.writeValueAsString(expectedBookDto);
-
         mvc.perform(delete(url).contentType(APPLICATION_JSON)
                         .with(user(user).authorities(new SimpleGrantedAuthority(role))))
                 .andExpect(status().is(result));
-    }
-
-    private List<BookDto> getTestListBookDto() {
-        BookDto bookDto1 = new BookDto(1L, "BookTitle_1",
-                new AuthorDto(1L, "Author_1"),
-                new GenreDto(1L, "Genre_1"));
-        BookDto bookDto2 = new BookDto(2L, "BookTitle_2",
-                new AuthorDto(2L, "Author_2"),
-                new GenreDto(2L, "Genre_2"));
-        BookDto bookDto3 = new BookDto(3L, "BookTitle_3",
-                new AuthorDto(3L, "Author_3"),
-                new GenreDto(3L, "Genre_3"));
-        return Arrays.asList(bookDto1, bookDto2, bookDto3);
     }
 }
